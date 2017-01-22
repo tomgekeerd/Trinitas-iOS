b30f5a8d733585b20ab6915c68ba0a9ab5f2d7b8
tomgekeerd <tomderuiter.hhw@hotmail.com>
2017-01-22 22:10:31 +0100

    Preps for mail data in view

--- Trinitas/Trinitas/MailViewController.swift
+++ Trinitas/Trinitas/MailViewController.swift
@@ -11,6 +11,7 @@ import SafariServices
 
 class MailViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
     @IBOutlet var setupButton: UIButton!
     @IBOutlet var setupLabel: UILabel!
     var sfViewController: SFSafariViewController!
@@ -20,9 +21,19 @@ class MailViewController: UIViewController {
 
         // Setup
         
-        self.setupButton.layer.cornerRadius = self.setupButton.frame.size.height / 2
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+
+        if !self.userDidSetupMail() {
+            
+            self.setupButton.layer.cornerRadius = self.setupButton.frame.size.height / 2
+            NotificationCenter.default.addObserver(self, selector: #selector(grantedViewControllerFinished(notification:)), name: Notification.Name(rawValue: "MailUpdate"), object: nil)
+            
+        } else {
+            
+            
+        }
         
-        NotificationCenter.default.addObserver(self, selector: #selector(grantedViewControllerFinished(notification:)), name: Notification.Name(rawValue: "MailUpdate"), object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,7 +41,8 @@ class MailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    // MARK: - View methods
+    
     func setupView(setup: Bool) {
         
         if setup {
@@ -47,6 +59,14 @@ class MailViewController: UIViewController {
         
     }
     
+    // MARK: - Helper methods
+    
+    func userDidSetupMail() -> Bool {
+        return UserDefaults().bool(forKey: "setupMail")
+    }
+    
+    // MARK: - IBActions
+    
     @IBAction func showSetupMail() {
         
         if let url = URL(string: "https://trinitas.itslearning.com/oauth2/authorize.aspx?client_id=10ae9d30-1853-48ff-81cb-47b58a325685&state=state&response_type=code&redirect_uri=itsl-itslearning://login&scope=SCOPE") {
@@ -59,14 +79,64 @@ class MailViewController: UIViewController {
 
 }
 
+// MARK: - UITableViewDelegate
+
+extension MailViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+
+extension MailViewController: UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MailCell
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+}
+
+// MARK: - MailCell
+
+class MailCell: UITableViewCell {
+    
+}
+
+// MARK: - SFSafariViewControllerDelegate
+
 extension MailViewController: SFSafariViewControllerDelegate {
     
     // MARK: - Get finish
     
     func grantedViewControllerFinished(notification: NSNotification) {
+        
+        // Check for data (refresh token)
+        
         if let code = notification.object as? String {
+            
+            // Set userdefault boolean
+            
+            UserDefaults().set(true, forKey: "setupMail")
+            UserDefaults().synchronize()
+            
+            // Dismiss the OAuth controller
+            
             self.sfViewController.dismiss(animated: true, completion: nil)
+            
         }
+        
     }
     
 }
